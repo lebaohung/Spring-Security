505a1a856b983077164d5925e03d56f8d0322191
lebaohung.1996 <lebaohung.1996@gmail.com>
2020-06-16 15:52:56 +0700

    add gitignore and README.md

---
+++ src/main/java/com/codegym/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.codegym.controller;
+
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+public class UserController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user(Principal principal){
+        System.out.println(principal.getName());
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        System.out.println(securityContext.getAuthentication().getName());
+        return "admin";
+    }
+}
